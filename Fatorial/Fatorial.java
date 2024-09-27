de9eb0b59176249e96a028d460a70b791ed7abd9
lcostaju <luciocj_eng@outlook.com>
2024-09-27 20:01:07 -0300

    aula recurssão

---
+++ Fatorial/Fatorial.java
@@ -0,0 +1,7 @@
+package Fatorial;
+
+public class Fatorial {
+    public static void main(String[] args) {
+        
+    }
+}
--- Fibonacci, Quantas Chamadas/src/App.java
+++ Fibonacci, Quantas Chamadas/src/App.java
@@ -1,5 +1,23 @@
+
+import java.util.Scanner;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Digite a posição da série de fibonacci?");
+        int n = s.nextInt();
+        int r = calcularFib(n);  
+        System.out.println("O valor da posição " + n + " é " + r);  
+        s.close();    
+    }
+
+    public static int calcularFib(int n){
+       if (n==0){
+            return 0;
+       }else if (n==1){
+            return 1;
+       }else{
+            return calcularFib(n-1) + calcularFib(n-2);
+       }
     }
 }
